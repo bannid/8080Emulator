cd2223f32e65942a45f19eac6461fc6257f97d33
Banni <navjotsingh.dhaliwal@connect.qut.edu>
2018-09-27 15:50:16 +1000

    add the cpu and flags stuct

--- Emulator/Emulator.c
+++ Emulator/Emulator.c
@@ -1,11 +1,34 @@
 #include "Emulator.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 int Decoder(unsigned char*);
 void InstructionNotImplemented(unsigned char*);
 void Emulate(unsigned char*);
-
+struct Flags{
+    uint8_t z:1;
+    uint8_t s:1;
+    uint8_t p:1;
+    uint8_t cy:1;
+    uint8_t ac:1;
+    uint8_t pad:3;
+}Flags;
+struct State8080{
+    uint8_t a;
+    uint8_t b;
+    uint8_t c;
+    uint8_t d;
+    uint8_t e;
+    uint8_t h;
+    uint8_t l;
+    uint16_t sp;
+    uint16_t pc;
+    uint8_t * memory;
+    struct Flags flags;
+    int interrup_enabled;
+}State8080;
 void Emulate(unsigned char * fileName){
+    struct State8080 cpu;
     FILE * filePointer = fopen(fileName, "rb");
 	if (filePointer == NULL)    
     {    
@@ -15,15 +38,14 @@ void Emulate(unsigned char * fileName){
     fseek(filePointer, 0L, SEEK_END);    
     int fileSize = ftell(filePointer);    
     fseek(filePointer, 0L, SEEK_SET);  
-    //Buffer to hold all the code
-    unsigned char * buffer = malloc(fileSize);
-    fread(buffer, fileSize, 1, filePointer); 
+    cpu.memory = malloc(fileSize);
+    fread(cpu.memory, fileSize, 1, filePointer); 
     fclose(filePointer);
-    //Temp program counter
-    int pc = 0;
-    while(pc < fileSize){
-    	pc += Decoder(buffer);
-    	buffer += pc;
+    cpu.pc = 0;
+    //Todo: Refactor this
+    while(cpu.pc < fileSize){
+    	cpu.pc += Decoder(cpu.memory);
+    	cpu.memory += cpu.pc;
     }
 }
 
